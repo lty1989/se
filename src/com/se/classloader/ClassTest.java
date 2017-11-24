dd0c1eaebdeeb9dca211ac6aa4431f8a1eb09038
lty <liutianyi_1989@163.com>
2017-11-24 22:41:22 +0800

    反射

--- src/com/se/classloader/ClassTest.java
+++ src/com/se/classloader/ClassTest.java
@@ -2,6 +2,7 @@ package com.se.classloader;
 
 import java.lang.annotation.*;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 
 
 @interface Anno {
@@ -41,7 +42,7 @@ public class ClassTest {
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchMethodException {
         Class<ClassTest> clazz = ClassTest.class;
         Constructor[] ctors = clazz.getDeclaredConstructors();
         System.out.println("全部构造方法");
@@ -51,6 +52,17 @@ public class ClassTest {
         Constructor[] publicCtors = clazz.getConstructors();
         System.out.println("全部public构造方法");
         for (Constructor c : publicCtors) {
+            System.out.println(c);
+        }
+        Method[] mtds = clazz.getMethods();
+        System.out.println("全部public方法");
+        for (Method m : mtds) {
+            System.out.println(m);
+        }
+        System.out.println("一个带String参数的info:" + clazz.getMethod("info", String.class));
+        Class<?>[] inners = clazz.getDeclaredClasses();
+        System.out.println("全部内部类");
+        for (Class c : inners) {
             System.out.println(c);
         }
 
